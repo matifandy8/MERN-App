6cae9cbf7c05a214cd497aadd9f6af9213587f80
matifandy <matifandy@gmail.com>
2020-12-26 22:03:44 -0300

    complete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,72 +1,80 @@
-import mongoose from "mongoose";
-import PostMessage from "../models/postMessage.js";
+import express from 'express';
+import mongoose from 'mongoose';
 
-export const getPosts = async (req, res) => {
-  try {
-    const postMessages = await PostMessage.find();
+import PostMessage from '../models/postMessage.js';
 
-    console.log(postMessages);
+const router = express.Router();
 
-    res.status(200).json(postMessages);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
+export const getPosts = async (req, res) => { 
+    try {
+        const postMessages = await PostMessage.find();
+                
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
 
 export const createPost = async (req, res) => {
-  const post = req.body;
+    const { title, message, selectedFile, creator, tags } = req.body;
 
-  const newPost = new PostMessage(post);
-  try {
-    await newPost.save();
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
 
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(409).json({ message: error.message });
-  }
-};
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
-  const post = req.body;
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-  if (!mongoose.Types.ObjectId.isValid(_id))
-    return res.status(404).send("No post with that id");
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(
-    _id,
-    { ...post, _id },
-    {
-      new: true,
-    }
-  );
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
-  res.json(updatedPost);
-};
+    res.json(updatedPost);
+}
 
 export const deletePost = async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send("No post with that id");
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-  await PostMessage.findByIdAndRemove(id);
+    await PostMessage.findByIdAndRemove(id);
 
-  res.json({ message: "Post deleted successfully" });
-};
+    res.json({ message: "Post deleted successfully." });
+}
 
 export const likePost = async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send("No post with that id");
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
 
-  const post = await PostMessage.findById(id);
-  const updatePost = await PostMessage.findByIdAndUpdate(
-    id,
-    { likeCount: post.likeCount + 1 },
-    { new: true }
-  );
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
+}
 
-  res.json(updatePost);
-};
+
+export default router;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,15 +13,17 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+const CONNECTION_URL =
+  "mongodb+srv://matifandy:yacuy1647@cluster0.aztoi.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
-
-// https://mongodb.com/cloud/atlas
